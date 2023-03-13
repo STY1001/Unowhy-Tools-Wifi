b663ebbaab718a62b0b78f93dfab7c3c2fa61809
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-03-13 21:02:34 +0100

    Add project files.

---
+++ Unowhy Tools Wifi/Main.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Unowhy_Tools_Wifi
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.serial = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serial
+            // 
+            this.serial.Location = new System.Drawing.Point(13, 13);
+            this.serial.Name = "serial";
+            this.serial.Size = new System.Drawing.Size(378, 22);
+            this.serial.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1291, 343);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1316, 397);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.serial);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Unowhy Tools Wifi";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox serial;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Unowhy Tools Wifi/Main.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Web.UI.Design.WebControls;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+namespace Unowhy_Tools_Wifi
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient();
+            string sn = serial.Text;
+            string g = wc.DownloadString($"https://idf.hisqool.com/conf/devices/{sn}/configuration");
+            string jsonString = g;
+            List<dynamic> dataList = JsonConvert.DeserializeObject<List<dynamic>>(jsonString);
+
+            List<string> urlList = new List<string>();
+
+            foreach (var data in dataList)
+            {
+                string url = data.url;
+                urlList.Add(url);
+            }
+
+            // Supposons que vous avez une liste de réponses JSON sous forme de chaînes de caractères
+            List<string> jsonResponseList = new List<string>();
+
+            // Créer un objet JObject vide pour stocker les informations fusionnées
+            JObject mergedJson = new JObject();
+
+            foreach (var url in urlList)
+            {
+                var client = new WebClient();
+                JObject json = JObject.Parse(client.DownloadString(url));
+                mergedJson.Merge(json);
+            }
+            
+            DataTable dt = new DataTable();
+            dt.Columns.Add("SSID", typeof(string));
+            dt.Columns.Add("Password", typeof(string));
+            dt.Columns.Add("Security Type", typeof(string));
+            dt.Columns.Add("Hidden", typeof(string));
+            dt.Columns.Add("Proxy Type", typeof(string));
+            dt.Columns.Add("Proxy URL (Automatic)", typeof(string));
+            dt.Columns.Add("Proxy Address (Manual)", typeof(string));
+            dt.Columns.Add("Proxy Port (Manual)", typeof(string));
+
+            
+
+            foreach (JProperty property in mergedJson.Properties())
+            {
+                if (property.Name.StartsWith("conf/network/all/"))
+                {
+                    DataRow row = dt.NewRow();
+                    JToken payload = property.Value["payload"];
+                    JToken options = property.Value["options"];
+                    JToken proxy = options["proxy"];
+                    if (payload != null && options != null && proxy != null)
+                    {
+                        Console.WriteLine("SSID: " + options["ssid"]);
+                        Console.WriteLine("Password: " + options["password"]);
+                        Console.WriteLine("Security: " + options["securityType"]);
+                        Console.WriteLine("Hidden: " + options["hidden"]);
+                        Console.WriteLine("Proxy: " + proxy["type"]);
+
+                        row["SSID"] = options["ssid"];
+                        row["Password"] = options["password"];
+                        row["Security Type"] = options["securityType"];
+                        row["Hidden"] = options["hidden"];
+                        row["Proxy Type"] = proxy["type"];
+
+                        if (proxy["type"].ToString() == "none")
+                        {
+                            Console.WriteLine("No proxy");
+                        }
+                        else if (proxy["type"].ToString() == "manual")
+                        {
+                            Console.WriteLine("Manual Proxy");
+                            Console.WriteLine("Port: " + proxy["proxyPort"]);
+                            Console.WriteLine("IP: " + proxy["proxyHostName"]);
+                            row["Proxy Address (Manual)"] = proxy["proxyHostName"];
+                            row["Proxy Port (Manual)"] = proxy["proxyPort"];
+                        }
+                        else if (proxy["type"].ToString() == "automatic")
+                        {
+                            Console.WriteLine("Auto Proxy");
+                            Console.WriteLine("URL: " + proxy["autoProxyUrl"]);
+                            row["Proxy URL (Automatic)"] = proxy["autoProxyUrl"];
+                        }
+                        
+                        dt.Rows.Add(row);
+                    }
+
+                    
+                }
+            }
+            
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
